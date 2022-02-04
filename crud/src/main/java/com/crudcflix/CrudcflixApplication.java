9e9cc3075182d9d18dc80ac20429d38083a0ef48
Marcela Silverio <marcelapsilverio@gmail.com>
2022-02-04 12:23:09 -0300

    arquivo renomeado

--- crud/src/main/java/com/crudcflix/CrudcflixApplication.java
+++ crud/src/main/java/com/crudcflix/CrudcflixApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EventosappApplication {
+public class CrudcflixApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EventosappApplication.class, args);
+        SpringApplication.run(CrudcflixApplication.class, args);
     }
 
 }
--- crud/src/main/java/com/crudcflix/DataConfiguration.java
+++ crud/src/main/java/com/crudcflix/DataConfiguration.java
@@ -13,7 +13,7 @@ public class DataConfiguration {
     @Bean
     public DriverManagerDataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/cflix?useTimezone=true&serverTimezone=UTC");
         dataSource.setUsername("root");
         dataSource.setPassword("marcela891260");
